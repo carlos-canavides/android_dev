45ed5d642b146afee3e1c574f9c77d0948a97773
carlos-canavides <cjcc1802@gmail.com>
2022-06-12 18:32:44 -0300

    add LOGIN logic
    Login in operation, you can only enter with admin user

--- app/src/main/java/com/example/androidapp/HomeActivity.kt
+++ app/src/main/java/com/example/androidapp/HomeActivity.kt
@@ -1,21 +1,33 @@
 package com.example.androidapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import com.google.android.material.navigation.NavigationView
+import com.google.firebase.auth.FirebaseAuth
 
-class MainActivity : AppCompatActivity() {
+enum class ProviderType {
+    BASIC
+}
+
+class HomeActivity : AppCompatActivity() {
+
     private lateinit var topAppBar: Toolbar
     private lateinit var toggle: ActionBarDrawerToggle
+    private var activity: Activity = this
+    private lateinit var text_username: TextView
+    private lateinit var text_email: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
         topAppBar = findViewById(R.id.topAppBar)
         setSupportActionBar(topAppBar)
         val drawer_layout: DrawerLayout = findViewById(R.id.drawer_layout)
@@ -23,15 +35,31 @@ class MainActivity : AppCompatActivity() {
         toggle = ActionBarDrawerToggle(this, drawer_layout, topAppBar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
         drawer_layout.addDrawerListener(toggle)
         toggle.syncState()
+        val header: View = nav_drawer.getHeaderView(0)
+        text_username = header.findViewById(R.id.user_name)
+        text_email = header.findViewById(R.id.user_email)
+        val bundle: Bundle? = intent.extras
+        val email: String? = bundle?.getString("email")
+        val provider: String? = bundle?.getString("provider")
+        if (email!=null && provider!=null) {
+            setUp(email, provider)
+        }
         initNavComponent(nav_drawer)
-
     }
 
     private fun initNavComponent(nav: NavigationView) {
         nav.setNavigationItemSelectedListener(NavigationView.OnNavigationItemSelectedListener { menuItem ->
             var selectedFragment: Fragment? = null
             var titulo: String = "Android"
-            when (menuItem.itemId) {
+            when(menuItem.itemId){
+                R.id.menu_logout -> {
+                    FirebaseAuth.getInstance().signOut()
+                    activity.finish()
+                };
+                R.id.menu_home -> {
+
+                };
+                R.id.menu_list -> {};
             }
             setTitle(titulo)
             replaceFragment(selectedFragment)
@@ -44,5 +72,10 @@ class MainActivity : AppCompatActivity() {
             val manager: FragmentManager = supportFragmentManager
             manager.beginTransaction().replace(R.id.fragment_container, selectedFragment).commit()
         }
+    }
+
+    private fun setUp(email: String, provider: String) {
+        text_username.setText("Administrador")
+        text_email.setText(email)
     }
 }
--- app/src/main/java/com/example/androidapp/LoginActivity.kt
+++ app/src/main/java/com/example/androidapp/LoginActivity.kt
@@ -1,13 +1,15 @@
 package com.example.androidapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.CheckBox
 import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
-
     private lateinit var edit_email: EditText
     private lateinit var edit_password: EditText
     private lateinit var checkbox_keep: CheckBox
@@ -20,5 +22,42 @@ class LoginActivity : AppCompatActivity() {
         edit_password = findViewById(R.id.login_password)
         checkbox_keep = findViewById(R.id.login_keep)
         button_signin = findViewById(R.id.login_button_signin)
+        setUp()
+    }
+
+    private fun setUp() {
+        button_signin.setOnClickListener {
+            if(edit_email.text.isNotEmpty() && edit_password.text.isNotEmpty()){
+                val email = edit_email.text
+                val password = edit_password.text
+                FirebaseAuth.getInstance().signInWithEmailAndPassword(email.toString(), password.toString())
+                    .addOnCompleteListener{
+                        if(it.isSuccessful) {
+                            showHome(it.result?.user?.email?: "", ProviderType.BASIC)
+                        }
+                        else {
+                            showAlert()
+                        }
+                    }
+
+            }
+        }
+    }
+
+    private fun showAlert() {
+        val builer = AlertDialog.Builder(this)
+        builer.setTitle("Error")
+        builer.setMessage("Se ha producido un error de autenticando al usuario")
+        builer.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog = builer.create()
+        dialog.show()
+    }
+
+    private fun showHome(email: String, provider: ProviderType) {
+        val homeIntent = Intent(this, HomeActivity::class.java).apply {
+            putExtra("email", email)
+            putExtra("provider", provider.name)
+        }
+        startActivity(homeIntent)
     }
 }
